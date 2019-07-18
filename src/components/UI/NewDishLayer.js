290d9bdb0fd3dc2ba4776029123225d0d1c655c5
Sierocki <sierockp@emea.roche.com>
2019-07-18 15:37:24 +0200

    Styled component for keyframes added

--- src/components/UI/NewDishLayer.js
+++ src/components/UI/NewDishLayer.js
@@ -1,34 +1,46 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
 
-import { makeStyles } from "@material-ui/core/styles";
-
-const styles = makeStyles({
-  box: {
-    position: "absolute",
-    fontSize: "9px",
-    top: "5px",
-    right: "-30px",
-    transform: "rotate(45deg)",
-    background: "#0066cc",
-    padding: ".2rem 2rem .2rem 2rem",
-    zIndex: "999"
-  },
-  boxSpan: {
-    color: "#fff",
-    letterSpacing: "1px"
+const pulse = keyframes`
+  0% {
+    transform: rotate(45deg) scale(1);
+    box-shadow: 0 1px 2px #000;
   }
-});
+  50% {
+    transform: rotate(45deg) scale(1.1);
+    box-shadow: 2px 2px 8px #000;
+  }
+  100% {
+    transform: rotate(45deg) scale(1);
+    box-shadow: 0 1px 2px #000;
+  }
+`;
+
+const Box = styled.div`
+  position: absolute;
+  font-size: 9px;
+  top: 5px;
+  right: -30px;
+  animation: 1.5s ${pulse} ease-out infinite;
+  background: #0066cc;
+  padding: 0.2rem 2rem 0.2rem 2rem;
+  z-index: 999;
+`;
+
+const StyledSpan = styled.div`
+  color: #fff;
+  letter-spacing: 1px;
+`;
 
 export default function NewDishlayer(props) {
   const { date } = props;
-  const classes = styles();
 
   const render = date => {
     if (new Date(date).toDateString() === new Date().toDateString())
       return (
-        <div className={classes.box}>
-          <span className={classes.boxSpan}>NEW</span>
-        </div>
+        <Box>
+          <StyledSpan>NEW</StyledSpan>
+        </Box>
       );
 
     return null;
