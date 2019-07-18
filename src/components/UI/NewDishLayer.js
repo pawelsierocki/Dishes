4056694756c46f6473630d55f0ef517d4a7717a1
Sierocki <sierockp@emea.roche.com>
2019-07-18 15:53:56 +0200

    NewDishLayer refactored with styled components

--- src/components/UI/NewDishLayer.js
+++ src/components/UI/NewDishLayer.js
@@ -3,16 +3,25 @@ import styled, { keyframes } from "styled-components";
 
 const pulse = keyframes`
   0% {
-    transform: rotate(45deg) scale(1);
-    box-shadow: 0 1px 2px #000;
+    transform: rotate(45deg);
+    box-shadow: 0 1px 3px #000;
+    top: 7px;
+    right: -30px;
+    font-size: 9px;
   }
   50% {
-    transform: rotate(45deg) scale(1.1);
-    box-shadow: 2px 2px 8px #000;
+    transform: rotate(45deg);
+    box-shadow: 1px 2px 8px #000;
+    top: 10px;
+    right: -27px;
+    font-size: 11px;
   }
   100% {
-    transform: rotate(45deg) scale(1);
-    box-shadow: 0 1px 2px #000;
+    transform: rotate(45deg);
+    box-shadow: 0 1px 3px #000;
+    top: 7px;
+    right: -30px;
+    font-size: 9px;
   }
 `;
 
@@ -21,7 +30,7 @@ const Box = styled.div`
   font-size: 9px;
   top: 5px;
   right: -30px;
-  animation: 1.5s ${pulse} ease-out infinite;
+  animation: 2s ${pulse} infinite;
   background: #0066cc;
   padding: 0.2rem 2rem 0.2rem 2rem;
   z-index: 999;
