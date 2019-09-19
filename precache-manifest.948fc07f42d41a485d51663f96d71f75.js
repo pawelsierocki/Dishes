3761a2a934c4bc70882bf8180c45f1e850a88edf
Sierocki <sierockp@emea.roche.com>
2019-09-19 10:33:37 +0200

    Updates

---
+++ precache-manifest.948fc07f42d41a485d51663f96d71f75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8a32b04f283d72e4613fb5fa9f9bef",
+    "url": "/Dishes/index.html"
+  },
+  {
+    "revision": "a67f7e6d7f045baa2413",
+    "url": "/Dishes/static/css/main.9854047a.chunk.css"
+  },
+  {
+    "revision": "77a6b8d574a5c4d58989",
+    "url": "/Dishes/static/js/2.86f9b92a.chunk.js"
+  },
+  {
+    "revision": "a67f7e6d7f045baa2413",
+    "url": "/Dishes/static/js/main.4ba2523a.chunk.js"
+  },
+  {
+    "revision": "28eb741fd6206baf95e3",
+    "url": "/Dishes/static/js/runtime~main.62305057.js"
+  }
+]);
